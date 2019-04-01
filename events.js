31927cb8eb1218a46afd1c30e73f25e656b290bf
HeathSilcox <chaxel.christophe@gmail.com>
2019-04-01 23:49:55 +0200

    Ajout de commentaires et de fioritures

--- events.js
+++ events.js
@@ -12,15 +12,28 @@ const couleurs = [
 
 function eventListener() {    
     for (let n = 0; n < couleurs.length; n++) {
+        // S'il n'y a pas de couleur définie, on commence par la premire couleur du tableau
         if (this.style.backgroundColor == '') {
-            this.style.backgroundColor = 'red';
-            break;
+            this.style.backgroundColor = couleurs[0];
+            this.innerHTML = `${couleurs[n]}`;
+            break;; // Evite de boucler sur toutes les valeurs inutiles.
         }
 
+        // Change la couleur actuelle, par la couleur suivante [Améliorable]
         if (this.style.backgroundColor == couleurs[n]) {
             this.style.backgroundColor = couleurs[n+1];
-            break;
+            this.innerHTML = `${couleurs[n+1]}`;
+
+            // Reset couleurs
+            if (n == couleurs.length - 1) {
+                this.style.backgroundColor = couleurs[0];
+                this.innerHTML = `${couleurs[0]}`;
+            }
+
+            break; // Permet de ne changer qu'une couleur à la fois.
         }
+
+ 
     }
 }
 
